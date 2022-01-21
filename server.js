fcb6b85e22dd3711506d970d731ecd6f5befbde9
Jamal Hall <jamalhallwebdev@gmail.com>
2022-01-21 18:00:15 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//server set up variables
 const express = require('express')
 const connectDB = require('./config/database')
 const app = express()
@@ -7,3 +8,19 @@ const PORT = process.env.PORT || 8000
 // router variables
 const homeRoutes = require('./routes/home')
 const todoRoutes = require('./routes/runTodos')
+
+//DB connection
+dotenv.config({path: './config/.env'})
+connectDB()
+
+// Set EJS as templating engine
+app.set('view engine','ejs')
+
+//express configurations
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+//router routes
+app.use('/', homeRoutes)
+app.use('/runTodos',)
