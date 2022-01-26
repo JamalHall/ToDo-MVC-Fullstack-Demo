72153b4045d583e8e18f08b433729fa982c93ef7
Jamal Hall <jamalhallwebdev@gmail.com>
2022-01-26 10:38:06 -0500

    get and post complete

--- controllers/todosCont.js
+++ controllers/todosCont.js
@@ -1,33 +1,40 @@
 const todoMod = require('../models/todoModel')
+const moment = require('moment')
+
 
 module.exports={
 
-        getTodo: async (req,res)=>{
-            try{
-                console.log('getTodo')
+    getTodo: async (req,res)=>{
+            try{                 
                 const todoItems = await todoMod.find()
                 const itemsLeft = await todoMod.countDocuments({complete: false})
-                res.render('todo.ejs', {todos: todoItems, left: itemsLeft} )
+                const date = moment(todoItems.date).format("MMM Do YY")
+                console.log(date)
+                res.render('todo.ejs',  {todoItems: todoItems, left: itemsLeft, newDate: date})
             }catch(err){ console.error(err)}
         },
 
-        createTodo: async (req,res)=>{
+    createTodo: async (req,res)=>{
+            console.log(req.body)
             try{
-                console.log('createTodo')
+                //console.log('createTodo')
+                await todoMod.create({task: req.body.todoFormItem, date: req.body.formDate, complete: false})
+                console.log('Todo has been added!',req.body)
+                res.redirect('/todos')
             }catch(err){ console.error(err)}
         },
 
-        completeTodo: async (req,res)=>{
+    completeTodo: async (req,res)=>{
             try{
-                console.log('completeTodo')
+                //console.log('completeTodo')
             }catch(err){ console.error(err)}
         },
 
-        deleteTodo: async (req,res)=>{
+    deleteTodo: async (req,res)=>{
             try{
-                console.log('deleteTodo')
+                //console.log('deleteTodo')
             }catch(err){ console.error(err)}
         }
-}
-   
 
+        
+}
--- models/todoModel.js
+++ models/todoModel.js
@@ -2,12 +2,18 @@ const mongoose = require('mongoose')
 
 const dbSchema = new mongoose.Schema({
     task:{
+        type: String,
+        required: true,
 
     },
     complete:{
+        type: Boolean,
+        required: true,
 
     },
     date:{
+        type: String,
+        required: true,
 
     }
 
